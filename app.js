4c815feef5c1f5d1ccd19efd6cd8d41fed5adef3
M. Talha Jabbar <muhammadtalha61940@gmail.com>
2021-12-19 18:52:24 +0500

    .env file added

--- app.js
+++ app.js
@@ -1,15 +1,25 @@
 const express = require('express');
 const app = express();
 
-const PORT = process.env.PORT || 5000;
-// the application will run locally on port 5000 but will use the environment variable on the Heroku servers.
+require('dotenv').config();
+
+const PORT = process.env.PORT || 5000; 
+/* 
+Project running locally:
+        The application will run locally on the port set in your .env file, if not set than will run on port 5000.
+Also,
+Project running on Hositing Services/Platforms(Production):
+        When hosting your application on another service (like Heroku, Nodejitsu, and AWS), your host may independently configure the 
+        process.env.PORT variable for you; after all, your script runs in their environment (thats why we have written process.env.PORT 
+        in 1st or left operand). But if they have not set it than your application will run on port 5000.
+*/
 
 const landlordRoutes = require('./routes/landlordRoutes');
 const tenantRoutes = require('./routes/tenantRoutes');
 
 const mongoose = require('mongoose');
 
-const dbURI = 'mongodb+srv://mtalha:test1234@nodetuts.2iktm.mongodb.net/tenant-app?retryWrites=true&w=majority';
+const dbURI = `mongodb+srv://${ process.env.DB_USER }:${ process.env.DB_PASSWORD }@nodetuts.2iktm.mongodb.net/${ process.env.DB_NAME }?retryWrites=true&w=majority`;
 
 mongoose.connect(dbURI)
     .then(() => {
@@ -21,4 +31,4 @@ app.use(express.urlencoded({ extended: false }));
 
 app.use('/api/landlords', landlordRoutes);
 
-app.use("/api/tenants", tenantRoutes);
+app.use('/api/tenants', tenantRoutes);
